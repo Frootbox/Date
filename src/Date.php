8377c953b0a1d0648f3d5385180476153d3cb29d
Jan Habbo Brüning <jan.habbo.bruening@gmail.com>
2019-08-27 14:22:19 +0200

    added Method Date::__toString()

--- src/Date.php
+++ src/Date.php
@@ -25,6 +25,15 @@ class Date {
     /**
      *
      */
+    public function __toString ( ): string
+    {
+        return date('Y-m-d H:i:s', $this->timestamp);
+    }
+
+
+    /**
+     *
+     */
     public function format ( $format ) {
 
         return strftime($format, $this->timestamp);
@@ -37,16 +46,32 @@ class Date {
     public function setDate ( $date ): Date {
 
         // Match date like 23.08.19 12:00
-        if (preg_match('#^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4}) ([0-9]{1,2})\:([0-9]{1,2})$#', $date, $match)) {
-
+        if (preg_match('#^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4}) ([0-9]{1,2})\:([0-9]{1,2})$#', $date, $match))
+        {
             $this->timestamp = mktime($match[4], $match[5], 0, $match[2], $match[1], $match[3]);
         }
+        // Match date like 23.08.19
+        elseif (preg_match('#^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4})$#', $date, $match))
+        {
+            $this->timestamp = mktime(0, 0, 0, $match[2], $match[1], $match[3]);
+        }
         // Match date like 2019-08-23 12:00:00
-        else if (preg_match('#^([0-9]{4})\-([0-9]{2})\-([0-9]{2}) ([0-9]{2})\:([0-9]{2})\:([0-9]{2})$#', $date, $match)) {
-
+        else if (preg_match('#^([0-9]{4})\-([0-9]{2})\-([0-9]{2}) ([0-9]{2})\:([0-9]{2})\:([0-9]{2})$#', $date, $match))
+        {
             $this->timestamp = mktime($match[4], $match[5], $match[6], $match[2], $match[3], $match[1]);
         }
-        else {
+        // Match date like 2019-08-23
+        else if (preg_match('#^([0-9]{4})\-([0-9]{1,})\-([0-9]{1,})$#', $date, $match))
+        {
+            $this->timestamp = mktime(0, 0, 0, $match[2], $match[3], $match[1]);
+        }
+        // Match date like 19-08-23
+        else if (preg_match('#^([0-9]{2})\-([0-9]{1,})\-([0-9]{1,})$#', $date, $match))
+        {
+            $this->timestamp = mktime(0, 0, 0, $match[2], $match[3], '20' . $match[1]);
+        }
+        else
+        {
             throw new \Frootbox\Exceptions\InputInvalid('Invalid date string: ' . $date);
         }
 
